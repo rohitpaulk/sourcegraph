da9a416ed463362e141f27302eb72adebc071698
Thorsten Ball <mrnugget@gmail.com>
2021-03-09 17:23:04 +0100

    Add conf parsing

---
+++ dev/sg/config.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
+func ParseConfigFile(name string) (*Config, error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot open file %q", name)
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, errors.Wrap(err, "reading configuration file")
+	}
+
+	var conf Config
+	if err := yaml.Unmarshal(data, &conf); err != nil {
+		return nil, err
+	}
+	return &conf, nil
+}
+
+type Command struct {
+	Cmd              string            `yaml:"cmd"`
+	Install          string            `yaml:"install"`
+	Env              map[string]string `yaml:"env"`
+	InstallDocDarwin string            `yaml:"install_doc.darwin"`
+	InstallDocLinux  string            `yaml:"install_doc.linux"`
+}
+
+type Config struct {
+	Commands    map[string]Command  `yaml:"commands"`
+	Commandsets map[string][]string `yaml:"commandsets"`
+	Tests       map[string]Command  `yaml:"tests"`
+}
--- dev/sg/main.go
+++ dev/sg/main.go
@@ -11,7 +11,9 @@ import (
 )
 
 var (
-	rootFlagSet  = flag.NewFlagSet("sg", flag.ExitOnError)
+	rootFlagSet = flag.NewFlagSet("sg", flag.ExitOnError)
+	configFlag  = rootFlagSet.String("config", "sg.config.yaml", "configuration file")
+
 	watchFlagSet = flag.NewFlagSet("sg watch", flag.ExitOnError)
 )
 
@@ -38,10 +40,30 @@ var rootCommand = &ffcli.Command{
 	ShortUsage:  "sg [flags] <subcommand>",
 	FlagSet:     rootFlagSet,
 	Subcommands: []*ffcli.Command{watchCommand},
+	Exec: func(ctx context.Context, args []string) error {
+		conf, err := ParseConfigFile(*configFlag)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("conf: %+v\n", conf)
+		return nil
+	},
 }
 
+var conf *Config
+
 func main() {
-	if err := rootCommand.ParseAndRun(context.Background(), os.Args[1:]); err != nil {
+	if err := rootCommand.Parse(os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
+
+	var err error
+	conf, err = ParseConfigFile(*configFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := rootCommand.Run(context.Background()); err != nil {
 		log.Fatal(err)
 	}
 }
